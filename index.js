7bf7ecae5a1de1259cf87bb8410ce079397cc0bd
sksingh2005 <shashanksgh3@gmail.com>
2025-02-18 00:02:34 +0530

    new change

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
+
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -6,6 +21,7 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
+    const { name, message } = req.query;
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
